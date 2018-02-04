0ccf6eddffbe82928590c01b936a99fc93a63d03
Varghese Abraham <varnit2010@gmail.com>
2018-02-05 00:55:20 +0530

    Added Google Authentication to app

--- ionic-google-auth-drive/src/pages/home/home.ts
+++ ionic-google-auth-drive/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GooglePlus } from '@ionic-native/google-plus';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private googlePlus: GooglePlus
+  ) {
 
+  }
+
+  signInWithGoogle() {
+    this.googlePlus.login({
+      'webClientId': '1004371791417-1fqtb5uppq99qdesjk85gonrfu24c9oi.apps.googleusercontent.com',
+      'offline': true
+    })
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
   }
 
 }
